a6dbbd0ba2781c7e6d5ced832948841da2d5e323
ms-cho91 <thirdxero@jj.ac.kr>
2024-06-05 16:48:44 +0900

    no message

--- v2/dijkstra/플로이드_워셜.py
+++ v2/dijkstra/플로이드_워셜.py
@@ -8,11 +8,15 @@ m=7
 # 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
+print(graph)
+
 # 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
 for a in range(1, n + 1):
     for b in range(1, n + 1):
         if a == b:
             graph[a][b] = 0
+
+print(graph)
 
 # 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
 # for _ in range(m):
@@ -37,12 +41,15 @@ graph = [
     [1000000000, 1000000000, 1000000000, 2, 0]
     ]
 
+print(graph)
 
 # 점화식에 따라 플로이드 워셜 알고리즘을 수행
 for k in range(1, n + 1):
     for a in range(1, n + 1):
         for b in range(1, n + 1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+print(graph)
 
 # 수행된 결과를 출력
 for a in range(1, n + 1):
