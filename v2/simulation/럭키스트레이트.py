b3e50989782731daa51c1ae8412c48b2a95828bf
ms-cho91 <thirdxero@jj.ac.kr>
2024-06-17 16:05:38 +0900

    no message

--- v2/simulation/럭키스트레이트.py
+++ v2/simulation/럭키스트레이트.py
@@ -31,16 +31,26 @@ n = '123402'
 
 number = len(n) // 2
 
+value=0
+
 left_value = 0
 for i in range(number) :
     left_value += int(n[i])
+    value += int(n[i])
 
 right_value = 0
 for i in range(number, len(n)) :
     right_value += int(n[i])
+    value -= int(n[i])
 
 if left_value == right_value :
     print('LUCKY')
 else :
     print('READY')
+
+if value == 0 :
+    print('LUCKY')
+else :
+    print('READY')
+
 
